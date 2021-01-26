a7928602fd71b2d1a9e8a5f47193a0343262624d
Marwa <majbrim@gmail.com>
2021-01-26 23:52:23 +0100

    transforming our script in class

--- read_api.py
+++ read_api.py
@@ -7,7 +7,7 @@ import pandas as pd
 import operator
 ######
 
-##### Reading the file with open
+##### Reading the file with opn
 '''
 json_data = None
 with open("stop_areas.json", 'r') as f:
@@ -34,18 +34,34 @@ with open('stop_areas.json', 'a') as f:
 #https://www.digitalocean.com/community/tutorials/getting-started-with-python-requests-get-requests
 
 
-logging.basicConfig(filename = "info_sncf.log", level= logging.INFO, format='%(asctime)s - %(name)s -%(levelname)s - %(message)s')
+class ReadingSncfApi():
 
-URL = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
-headers = {"Authorization": "318fcd11-c5f1-4180-8420-4994c3a5705e"}
-def main():
+    logging.basicConfig(filename = "info_sncf.log", 
+    level= logging.INFO, 
+    format='%(asctime)s - %(name)s -%(levelname)s - %(message)s')
 
-    def request_JSON():
+    logging.info('Request api -- start')
+        
+    def __init__(self):
+        self.URL = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
+        self.headers = {"Authorization": "318fcd11-c5f1-4180-8420-4994c3a5705e"}
+        self.raw_data = None
+        self.my_endpoints_list = []
+        self.my_id_list = []
+        self.my_list_station = []
+        self.my_list_coord = []
+        logging.info('Request api -- start')
+    
+
+    
+    def request_JSON(self):
+
+        
         try:
-            r = requests.get(url= URL, headers=headers)
-            
-            print(r.status_code)
-            raw_data = json.loads(r.text)
+            r = requests.get(url= self.URL, headers=self.headers)
+          
+            #print(r.status_code)
+            self.raw_data = json.loads(r.text)
             #print(type(raw_data))
             #pprint.pprint(raw_data)
             with open("marwa_sncf.json", mode="w+", encoding='utf-8') as f:
@@ -53,118 +69,97 @@ def main():
         except:
             logging.info('Error: file not found, cannot access the file')
         
-        return raw_data
-    def_request_json = request_JSON()
-    #pprint.pprint(def_request_json)
+        logging.info('Request api -- end')
 
-    def save_my_json():
-        with open("marwa_sncf.json") as file:   
-            data = json.load(file)
+        return self.raw_data
 
-    save_my_json()
+    def save_my_json(self):
+        self.my_log.info('saving json from api -- start')
 
+        try:
+            with open("marwa_sncf.json") as file:   
+                self.data = json.load(file)
+        except OSError:
+            logging.info("Error: file isn't saved")
 
-    def my_endpoints():
-        #my_json = save_my_json()
-        link = def_request_json['links']
-        #pprint.pprint(link)
-        my_endpoints_list = []
+        logging.info('saving json from api -- end')
 
-        for k, loop_endpoint in enumerate(link):
+    def my_endpoints(self):
+
+        self.link = self.raw_data['links']
+
+        for k, loop_endpoint in enumerate(self.link):
             if type(loop_endpoint) == dict:
                 if "href" in loop_endpoint.keys():
                     local_endpoint = loop_endpoint["href"]
-                    my_endpoints_list.append(local_endpoint)
+                    self.my_endpoints_list.append(local_endpoint)
                 else:
-                    logging.info("Currently in endpoints, the key 'href' is missing in %s" %k)
+                    self.my_log.info("Currently in endpoints, the key 'href' is missing in %s" %k)
             else:
-                logging.info("Currently in endpoints, format is not a dict, check condition")
+                self.my_log.info("Currently in endpoints, format is not a dict, check condition")
         
-        return my_endpoints_list
+        
 
-    def_endpoints = my_endpoints()
-    print(def_endpoints)
+    def my_id(self):
+        
+        self.areas = self.raw_data["stop_areas"]
 
-
-    def my_id():
-        # id
-        areas = def_request_json["stop_areas"]
-        #print('type of areas', type(areas))
-
-        my_id = []
-        for k, loop_id in enumerate(areas):
+        for k, loop_id in enumerate(self.areas):
             if type(loop_id) == dict:
                 if "id" in loop_id.keys():
                     local_id = loop_id["id"]
-                    my_id.append(local_id)
+                    self.my_id_list.append(local_id)
                 else:
                     logging.info("Currently in id, the key 'href' is missing in %s" %k)
             else:
                 logging.info("Currently in id, format is not a dict, check condition")
         
-        return my_id
 
-    def_id = my_id()
-    #print(def_id)
+    def my_station_name(self):
+        self.areas = self.raw_data["stop_areas"]
 
-    def my_station_name():
-        areas = def_request_json["stop_areas"]
-        my_gare = []
-
-        for k, loop_gare in enumerate(areas):
+        for k, loop_gare in enumerate(self.areas):
             if type(loop_gare) == dict:
                 if "label" in loop_gare.keys():
                     local_gare = loop_gare["label"]
-                    my_gare.append(local_gare)
+                    self.my_list_station.append(local_gare)
                 else:
                     logging.info("Currently in station name, the key 'href' is missing in %s" %k)
             else:
                 logging.info("Currently in station name, format is not a dict, check condition")
         
-        return my_gare
 
-    def_station_name = my_station_name()
-    #print(def_station_name)
+    def my_coord(self):
 
-    def my_coord():
-
-        areas = def_request_json["stop_areas"]
-        my_coord = []
+        areas = self.raw_data["stop_areas"]
 
         for k, loop_coord in enumerate(areas):
             if type(loop_coord) == dict:
                 if "coord" in loop_coord.keys():
                     local_coord = loop_coord["coord"]
-                    my_coord.append(local_coord)
+                    self.my_list_coord.append(local_coord)
                 else:
                     logging.info("Currently in coord, the key 'href' is missing in %s" %k)
             else:
-                logging.info("Currently in coord, format is not a dict, check condition")
-        
-        return my_coord
-
-    def_coord= my_coord()
-    #print(def_coord)
+                logging.info("Currently in coord format is not a dict, check condition")
 
 
-    def csv_station():
+   
+    def csv_station(self):
 
-        ############ Creating CSV
-        # CSV info station
-        data = {'id':def_id, 'name':def_station_name, 'coord':def_coord}
+        self.data = {'id':self.my_id_list, 'name':self.my_list_station, 'coord':self.my_list_coord}
 
-        info = pd.DataFrame(data)
+        info = pd.DataFrame(self.data)
         #print(info)
 
         with open('my_gare.csv', 'w') as f:
-            station_csv = info.to_csv(f, encoding='utf-8') 
-        
-        
-    csv_station()
+            self.station_csv = info.to_csv(f, encoding='utf-8') 
 
-    def create_csv_endpoints():
+        
+
+    def create_csv_endpoints(self):
         # CSV endpoints
-        links = {'endpooints': def_endpoints }
+        links = {'endpoints': self.my_endpoints_list}
         endpoint = pd.DataFrame(links)
         ##print(endpoint)
 
@@ -175,4 +170,19 @@ def main():
 
 
 
-main()
+if __name__ == '__main__':
+    logging.basicConfig(filename = "info_sncf.log", 
+    level= logging.INFO, 
+    format='%(asctime)s - %(name)s -%(levelname)s - %(message)s')
+    logging.info('Request api -- start')
+
+
+    my_class = ReadingSncfApi()
+    my_class.request_JSON()
+    my_class.my_endpoints()
+    my_class.my_id()
+    my_class.my_station_name()
+    my_class.my_coord()
+   #print(my_class.my_list_coord) #print pour afficher valeur
+    my_class.csv_station()
+    my_class.create_csv_endpoints()
